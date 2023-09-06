cd80998a1db779d67225bbbaba6b88340f0fcca6
kaulmesanyam <kaulsanyam@gmail.com>
2023-09-06 19:36:55 +0530

    added gitignore file and created UDPPingerModifiedServer.py

---
+++ Sem_1/Advanced_Computer_Networks/UDP_Pinger/Python/UDPPingerModifiedServer.py
@@ -0,0 +1,14 @@
+# UDPPingerServer.py
+from socket import *
+# Create a UDP socket
+# Notice the use of SOCK_DGRAM for UDP packets
+serverSocket = socket(AF_INET, SOCK_DGRAM)
+# Assign IP address and port number to socket
+serverSocket.bind(('', 12000))
+while True:
+    # Receive the client packet along with the address it is coming from
+    message, address = serverSocket.recvfrom(1024)
+    # Capitalize the message from the client
+    message = message.upper()
+    #the server responds
+    serverSocket.sendto(message, address)
