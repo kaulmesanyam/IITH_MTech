ac9ba30e67b6be875afecd4d16380196c25b076e
kaulmesanyam <kaulsanyam@gmail.com>
2023-09-08 22:33:06 +0530

    adding comments to code

--- Sem_1/Advanced_Computer_Networks/TCP_Pinger/Python/TCPPingerClient.py
+++ Sem_1/Advanced_Computer_Networks/TCP_Pinger/Python/TCPPingerClient.py
@@ -1,58 +1,68 @@
 import socket
 import time
 
+# Define the server address and port
 server_addr = ('127.0.0.1', 12000)
 
+# Get the number of pings to send from the user
 pings = int(input("Number of pings to send: "))
 
+# Create a TCP socket
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+# Initialize variables to track round-trip times (rtt) and lost packets
 rtt = []
 lost_packets = 0
 total_packets = 0
 
 try:
+    # Connect to the server
     client.connect(server_addr)
-    print("connection established with server")
+    print("Connection established with server")
 
-    while(pings > 0):
+    while pings > 0:
         for seq_no in range(1, pings + 1):
             timestamp = time.time()
             message = f'ping {seq_no} {timestamp}'
 
-            
+            # Send the message to the server
             client.sendall(message.encode())
-            total_packets += 1;
+            total_packets += 1
 
+            # Set a timeout for receiving a response from the server
             client.settimeout(1.0)
             while True:
                 try:
+                    # Try to receive a response from the server
                     response = client.recv(1024)
 
+                    # Calculate the round-trip time (RTT)
                     curr_rtt = time.time() - timestamp
                     rtt.append(curr_rtt)
 
+                    # Print the response and RTT
                     print(f'Response received from {server_addr}: {response.decode()} | RTT: {curr_rtt:.6f} seconds')
                     break  # Exit the inner loop on successful response
 
                 except socket.timeout:
+                    # Handle the case where the response times out
                     print(f'Ping {seq_no} has timed out -- re-transmitting same packet again')
                     lost_packets += 1
                     total_packets += 1
                     timestamp = time.time()
 
-
         if rtt:
-            min = min(rtt)
-            max = max(rtt)
-            avg = sum(rtt) / len(rtt)
+            min_rtt = min(rtt)
+            max_rtt = max(rtt)
+            avg_rtt = sum(rtt) / len(rtt)
             loss_rate = (lost_packets / total_packets) * 100
             print("\nStatistics:\n")
             print(f"  Packets sent: {total_packets}")
+            print(f"  Packets received: {total_packets - lost_packets}")
             print(f"  Packets lost: {lost_packets} ({loss_rate:.2f}% loss)")
-            print(f"  Minimum RTT: {min:.6f} seconds")
-            print(f"  Maximum RTT: {max:.6f} seconds")
-            print(f"  Average RTT: {avg:.6f} seconds")
+            print(f"  Minimum RTT: {min_rtt:.6f} seconds")
+            print(f"  Maximum RTT: {max_rtt:.6f} seconds")
+            print(f"  Average RTT: {avg_rtt:.6f} seconds")
         else:
             print("\nNo response received as all packets got lost\n")
 
@@ -64,11 +74,9 @@ try:
             pings = int(input("Number of pings to send: "))
 
 except Exception as e:
-    print(f"Error {e}")
+    print(f"Error: {e}")
 
 finally:
+    # Close the client socket
     client.close()
     print("Connection closed")
-
-
-
--- Sem_1/Advanced_Computer_Networks/TCP_Pinger/Python/TCPPingerServer.py
+++ Sem_1/Advanced_Computer_Networks/TCP_Pinger/Python/TCPPingerServer.py
@@ -1,35 +1,48 @@
 import random
 from socket import *
 
+# Create a TCP socket
 serverSocket = socket(AF_INET, SOCK_STREAM)
 
+# Bind the socket to a specific address and port
 serverSocket.bind(('', 12000))
+
+# Set the socket to listen for incoming connections
 serverSocket.listen()
 
 print("The server is ready to receive requests!")
 
 try:
+    # Accept incoming connection and get the client socket and address
     clientSocket, addr = serverSocket.accept()
     print(f"Connected to client at {addr}")
 
     while True:
-    
+        # Generate a random number between 0 and 11
         randomNum = random.randint(0, 11)
+
+        # Receive a message from the client and decode it
         msg = clientSocket.recv(1024).decode()
+
+        # Check if the received message is null
         if not msg:
             print("null message object received")
             break
 
+        # Convert the message to uppercase
         msg = msg.upper()
 
+        # Simulate packet loss based on random number
         if randomNum < 4:
             continue
 
+        # Send the modified message back to the client
         clientSocket.send(msg.encode())
 
 except Exception as e:
     print(f"Error: {e}")
 
 finally:
+    # Close the client socket
     clientSocket.close()
     print("Connection closed")
